1453bc67f58b18d0075e39961cb358c7ac2f69b1
belsy05 <belsy.mairena@unah.hn>
2022-02-15 23:30:20 -0600

    Update raizpersonal.blade.php

--- resources/views/raizpersonal.blade.php
+++ resources/views/raizpersonal.blade.php
@@ -38,5 +38,56 @@
     </div>
       
         <br>
-    
+        <table class="table table-bordered border-dark mt-3" >
+        <thead class="table table-striped table-hover">
+            <tr class="success">
+                <th scope="col">N°</th>
+                <th scope="col">Identidad</th>
+                <th scope="col">Nombres</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Teléfono</th>
+                <th scope="col">Fecha de Ingreso</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Más Detalles</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Cambiar Estado</th>
+            </tr>  
+        </thead>
+        <tbody>
+        @forelse ($personals as $personal)
+            <tr class="active">
+                <th scope="row">{{ $personal->id }}</th>
+                <td scope="col">{{ $personal->IdentidadPersonal }}</td>
+                <td scope="col">{{ $personal->NombrePersonal }}</td>
+                <td scope="col">{{ $personal->ApellidoPersonal }}</td>
+                <td scope="col">{{ $personal->Telefono}}</td>
+                <td scope="col">{{ $personal->FechaIngreso}}</td>
+                <td scope="col">
+                    @if ($personal->EmpleadoActivo)
+                        Activo
+                    @else
+                        Inactivo
+                    @endif
+                </td>
+                
+                <td> <a class="btn btn-success" href="{{ route('personal.mostrar',['id' => $personal->id]) }}" > Más Detalles </a></td>
+                <td> <a class="btn btn-success" href="{{ route('personal.edit',['id' => $personal->id]) }}"> Editar </a></td>
+                <td>
+                    @if ($personal->EmpleadoActivo)
+                        <a class="btn btn-danger" href="{{ route('status.update',['id' => $personal->id]) }}">Desactivar</a>
+                    @else
+                        <a class="btn btn-success" href="{{ route('status.update',['id' => $personal->id]) }}">Activar</a>
+                    @endif
+                </td>
+
+            </tr>
+        @empty
+            <tr>
+                <td colspan="4"> No hay más empleados </td>
+            </tr>    
+        @endforelse
+
+        </tbody>
+    </table>
+    {{ $personals->links()}}
 @endsection

fa7f82f764eaeb2a208081e8eab77060081700ca
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-02-16 21:35:55 -0600

    Create Personal.php

---
+++ app/Models/Personal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Personal extends Model
+{
+    use HasFactory;
+
+    public function cargo(){
+        return $this->belongsTo(Cargo::class);
+    }
+}

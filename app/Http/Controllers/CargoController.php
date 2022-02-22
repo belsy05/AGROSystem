a8c550920d2de6b2609dc3859107cbb0a803cdc7
EstefaniHerrera <estefani.herrera@unah.hn>
2022-02-21 21:27:23 -0600

    Update CargoController.php

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -1,3 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Validation\Rule;
+
+use App\Models\Cargo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CargoController extends Controller
+{
+    public function index(){
+        $cargo = Cargo::paginate(10);
+        return view('cargos.raizcargos')->with('cargos', $cargo);
+    }
+
+    //funcion para la barra
+    public function index2(Request $request){
+
+        $texto =trim($request->get('texto'));
+        $cargo = DB::table('cargos')
+                        ->where('NombreCargo', 'LIKE', '%'.$texto.'%')
+                        ->paginate(10);
+        return view('cargos.raizcargos')->with('cargos', $cargo);
+    }
+
+    //funcion para crear o insertar datos
+    public function crear(){
+        return view('cargos.formularioCargo');
+    }
+
+    //funcion para guardar los datos creados o insertados
+    public function store(Request $request){
+        //VALIDAR
+        $request->validate([
+            'NombreDelCargo'=>'required|unique:cargos|string|max:40|min:5',
+            'DescripciónDelCargo'=>'required|string|max:150|min:5',
+            'Sueldo'=>'required|numeric|min:1000.00|max:30000.00'
+        ]);
+
+        //Formulario
+        $nuevoCargo = new Cargo();
+        $nuevoCargo->NombreDelCargo = $request->input('NombreDelCargo');
+        $nuevoCargo->DescripciónDelCargo = $request->DescripciónDelCargo;
+        $nuevoCargo->Sueldo = $request->input('Sueldo');
+
+        $creado = $nuevoCargo->save();
+
+        if($creado){
+            return redirect()->route('cargo.index')
+                ->with('mensaje', 'El cargo fue creado exitosamente');
+        }else{
+            //retornar con un mensaje de error
+        }
+    }
+
 //funcion para editar los datos
     public function edit($id){
         $cargo = Cargo::findOrFail($id);

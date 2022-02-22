911676179f3a5e5cc97e8a366e54e032a13f3974
EstefaniHerrera <estefani.herrera@unah.hn>
2022-02-21 21:32:22 -0600

    Update formularioEditarCargo.blade.php

--- resources/views/cargos/formularioEditarCargo.blade.php
+++ resources/views/cargos/formularioEditarCargo.blade.php
@@ -1,47 +1,27 @@
-@extends('Plantillas.plantilla')
+@push('alertas')
 
-@section('titulo', 'Editar Cargos')
+<script>
+    function confirmar(id) {
+       var formul = document.getElementById("form_editarC");
 
-@section('contenido')
 
-<h1> Editar Cargos </h1>
-<br><br>
+        Swal.fire({
+            title: '¿Está seguro que desea actualizar los datos del cargo?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Aceptar'
+        }).then((result)=>{
+            if (result.isConfirmed) {
+                formul.submit();
+            }
 
-<!-- PARA LOS ERRORES -->
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+        })
 
-<form method="POST" action="">
-    @method('put')
-    @csrf <!-- PARA PODER ENVIAR EL FORMULARIO -->
+        event.preventDefault()
 
-    <div class="form-group">
-        <label for="NombreCargo"> Nombre </label>
-        <input type="text" class="form-control" name="NombreCargo" id="NombreCargo" placeholder="Nombre del cargo"
-        value="{{$cargo->NombreCargo}}">
-    </div>
 
-    <div class="form-group">
-        <label for="DescripcionCargo"> Descripción </label>
-        <textarea class="form-control" name="DescripcionCargo" id="DescripcionCargo" cols="30" rows="10" placeholder="Breve descripción de la función del puesto">{{$cargo->DescripcionCargo}}
-        </textarea>
-    </div>
-
-    <div class="form-group">
-        <label for="Sueldo"> Sueldo </label>
-        <input type="number" class="form-control" name="Sueldo" id="Sueldo" placeholder="00.00" value="{{$cargo->Sueldo}}">
-    </div>
-
-    <br>
-    <input type="submit" class="btn btn-primary" value="Actualizar" onclick="return confirm('¿Está seguro que desea actualizar los datos del cargo?')">
-    <input type="reset" class="btn btn-danger" value="Restaurar"> 
-    <a class="btn btn-info" href="{{route('cargo.index')}}">Cerrar</a>
-</form> 
-@endsection
+    }
+</script>
+@endpush

1d994d5571a8bd493f47da1f783b19c66f6703d2
belsy05 <belsy.mairena@unah.hn>
2022-02-21 21:19:29 -0600

    Update CargoController.php

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -1,59 +1,4 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Cargo;
-use Illuminate\Http\Request;
-
-class CargoController extends Controller
-{
-    public function index(){
-        $cargo = Cargo::paginate(10);
-        return view('cargos.raizcargos')->with('cargos', $cargo);
-    }
-
-    //funcion para la barra
-    /* public function index2(Request $request){
-        
-        $texto =trim($request->get('texto'));
-        $personals = DB::table('Personals')
-                        ->where('NombrePersonal', 'LIKE', '%'.$texto.'%')
-                        ->orWhere('cargo_id', 'LIKE', '%'.$texto.'%')
-                        ->paginate(10);
-        return view('buscarPersonal', compact('personals', 'texto'));
-    } */
-
-    //funcion para crear o insertar datos
-    public function crear(){
-        return view('cargos.formularioCargo');
-    }
-
-    //funcion para guardar los datos creados o insertados
-    public function store(Request $request){
-        //VALIDAR
-        $request->validate([
-            'NombreCargo'=>'required',
-            'DescripcionCargo'=>'required',
-            'Sueldo'=>'required'
-        ]);
-
-        //Formulario
-        $nuevoCargo = new Cargo();
-        $nuevoCargo->NombreCargo = $request->input('NombreCargo');
-        $nuevoCargo->DescripcionCargo = $request->DescripcionCargo;
-        $nuevoCargo->Sueldo = $request->input('Sueldo');
-
-        $creado = $nuevoCargo->save();
-
-        if($creado){
-            return redirect()->route('cargo.index')
-                ->with('mensaje', 'El cargo fue creado exitosamente');
-        }else{
-            //retornar con un mensaje de error
-        }
-    }
-
-    //funcion para editar los datos
+//funcion para editar los datos
     public function edit($id){
         $cargo = Cargo::findOrFail($id);
         return view('cargos.formularioEditarCargo')->with('cargo', $cargo);
@@ -63,15 +8,21 @@ class CargoController extends Controller
     //funcion para actualizar los datos
     public function update(Request $request, $id){
 
+        $cargo = Cargo::findOrFail($id);
         $request->validate([
-            'NombreCargo'=>'required',
-            'DescripcionCargo'=>'required',
-            'Sueldo'=>'required'
+            'NombreDelCargo'=> [
+                'required',
+                'string',
+                'max:40',
+                'min:5',
+                Rule::unique('cargos')->ignore($cargo->id),
+            ],
+            'DescripciónDelCargo'=>'required|string|max:150',
+            'Sueldo'=>'required|numeric|min:1000.00|max:30000.00'
         ]);
 
-        $cargo = Cargo::findOrFail($id);
-        $cargo->NombreCargo = $request->input('NombreCargo');
-        $cargo->DescripcionCargo = $request->DescripcionCargo;
+        $cargo->NombreDelCargo = $request->input('NombreDelCargo');
+        $cargo->DescripciónDelCargo = $request->DescripciónDelCargo;
         $cargo->Sueldo = $request->input('Sueldo');
 
         $creado = $cargo->save();
@@ -83,4 +34,3 @@ class CargoController extends Controller
             //retornar con un mensaje de error
         }
     }
-}

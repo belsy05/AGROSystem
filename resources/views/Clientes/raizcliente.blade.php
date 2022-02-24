420e722ee600731f6b53fd28febed8b7672f311b
EstefaniHerrera <estefani.herrera@unah.hn>
2022-02-24 00:07:13 -0600

    Create raizcliente.blade.php

---
+++ resources/views/Clientes/raizcliente.blade.php
@@ -0,0 +1,74 @@
+@extends('Plantillas.plantilla')
+
+@section('titulo', 'Clientes')
+@section('barra')
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12">
+            <form action="{{route('cliente.index2')}}" method="GET">
+                <div class="form-row">
+                    <div class="col-sm-4 my-1">
+                        <input type="search" class="form-control" name="texto" placeholder="Buscar">
+                    </div>
+                    <div class="col-auto my-1">
+                        <input type="submit" class="btn btn-secondary" value="Buscar">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
+@section('contenido')
+@if (session('mensaje'))
+        <div class="alert alert-success">
+            {{ session('mensaje') }}
+        </div>
+    @endif
+    <br>
+
+
+
+    <h1 class=""> Listado De Clientes </h1>
+    <br>
+    <div class="d-grid gap-2 d-md-block ">
+        <a class="btn btn-success float-" href="{{route('cliente.crear')}}"> Agregar Cliente </a>
+        <a class="btn btn-success float-end me-md-2" href=""> Regresar </a>
+    </div>
+
+        <br>
+
+
+
+
+    <table class="table table-bordered border-dark mt-3" >
+        <thead class="table table-striped table-hover">
+            <tr class="success">
+                <th scope="col">N°</th>
+                <th scope="col">N° De Identidad</th>
+                <th scope="col">Nombre Completo</th>
+                <th scope="col">Lugar De Procedencia</th>
+            </tr>
+        </thead>
+        <tbody>
+        @forelse ($clientes as $cliente)
+            <tr class="active">
+                <th scope="row">{{ $cliente->id }}</th>
+                <td scope="col">{{ $cliente->IdentidadDelCliente}}</td>
+                <td scope="col">{{ $cliente->NombresDelCliente}} {{ $cliente->ApellidosDelCliente}}</td>
+                <td scope="col">{{ $cliente->LugarDeProcedencia }}</td>
+                <td> <a class="btn btn-success" href="{{ route('cliente.mostrar',['id' => $cliente->id]) }}" > Más Detalles </a></td>
+                <td> <a class="btn btn-success" href="{{ route('cliente.edit',['id' => $cliente->id]) }}"> Editar </a></td>
+
+            </tr>
+        @empty
+            <tr>
+                <td colspan="4"> No hay más clientes </td>
+            </tr>
+        @endforelse
+
+        </tbody>
+    </table>
+    {{ $clientes->links()}}
+
+@endsection

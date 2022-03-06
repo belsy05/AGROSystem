a5961d4addb0a8aa1cbecfd86da3d933c7cf25c8
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-03-06 00:06:19 -0600

    Create ProductoController.php

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+
+
+    //funcion para crear o insertar datos
+    public function crear(){
+        $categorias = Categoria::all();
+        return view('Productos.formularioProducto', compact('categorias'));
+    }
+
+    //funcion para guardar los datos creados o insertados
+    public function store(Request $request){
+        //VALIDAR
+        $request->validate([
+            'Categoria'=>'required',
+            'CódigoDelProducto'=>'required|unique:productos|max:8',
+            'NombreDelProducto'=>'required||max:20',
+            'DescripciónDelProducto'=>'required|string|max:150|min:10',
+            'PresentaciónDelProducto'=>'required|max:30',
+            'Impuesto'=>'required|max:10',
+            'FechaDeElaboración'=>'required|date',
+            'FechaDeVencimiento'=>'required|date',
+        ]);
+
+        //Formulario
+        $nuevoProducto = new Producto();
+        $nuevoProducto->categoria_id = $request->categoria;
+        $nuevoProducto->CódigoDelProducto = $request->input('CódigoDelProducto');
+        $nuevoProducto->NombreDelProducto = $request->input('NombreDelProducto');
+        $nuevoProducto->DescripciónDelProducto = $request->input('DescripciónDelProducto');
+        $nuevoProducto->PresentaciónDelProducto = $request->input('PresentaciónDelProducto');
+        $nuevoProducto->Impuesto = $request->input('Impuesto');
+        $nuevoProducto->FechaDeElaboración = $request->input('FechaDeElaboración');
+        $nuevoProducto->FechaDeVencimiento = $request->input('FechaDeVencimiento');
+        $creado = $nuevoProducto->save();
+
+        if($creado){
+            return redirect()->route('producto.index')
+                ->with('mensaje', 'El producto fue creado exitosamente');
+        }else{
+            //retornar con un mensaje de error
+        } 
+    }
+}

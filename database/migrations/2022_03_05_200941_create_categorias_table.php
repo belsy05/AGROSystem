348c12929e6289eade6cc83e0cccad770674d90c
EstefaniHerrera <estefani.herrera@unah.hn>
2022-03-06 19:58:59 -0600

    Update 2022_03_05_200941_create_categorias_table.php

--- database/migrations/2022_03_05_200941_create_categorias_table.php
+++ database/migrations/2022_03_05_200941_create_categorias_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('NombreDeLaCategoría');
+            $table->string('NombreDeLaCategoría')->unique();
             $table->text('DescripciónDeLaCategoría');
             $table->timestamps();
         });

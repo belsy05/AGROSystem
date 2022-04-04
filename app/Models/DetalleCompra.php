b79d8210a4195810c0eb659c62a09af38bebf44d
maryury-chacon <maryury.chacon@unah.hn>
2022-04-03 19:39:47 -0600

    Modelos Compra y Detalle de compra

---
+++ app/Models/DetalleCompra.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleCompra extends Model
+{
+    use HasFactory;
+
+
+    public function producto()
+    {
+        return $this->hasOne(Producto::class,'id','IdProducto');
+    }
+
+    public function presentacion(){
+        return $this->belongsTo(Presentacion::class, 'IdPresentacion');
+    }
+}

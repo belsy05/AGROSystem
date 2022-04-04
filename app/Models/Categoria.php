f1686e6eeab1d94f1fe96179b4028a76de3e3873
EstefaniHerrera <estefani.herrera@unah.hn>
2022-04-03 20:29:55 -0600

    Update Categoria.php

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -12,4 +12,8 @@ class Categoria extends Model
         return $this->hasMany(Producto::class, 'id');
     }
 
+    public function presentacion(){
+        return $this->hasMany(Presentacion::class, 'id');
+    }
+
 }

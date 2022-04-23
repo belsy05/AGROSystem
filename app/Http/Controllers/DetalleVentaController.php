2da6eb12df539cba3998aaef53c08f83372d1908
belsy05 <belsy.mairena@unah.hn>
2022-04-23 01:17:06 -0600

    Create DetalleVentaController.php

---
+++ app/Http/Controllers/DetalleVentaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetalleVenta;
+use App\Models\Inventario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DetalleVentaController extends Controller
+{
+   
+}

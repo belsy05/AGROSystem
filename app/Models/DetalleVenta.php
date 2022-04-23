9047711dfbf0e12588e9d9dcd7f555ad112c7b74
belsy05 <belsy.mairena@unah.hn>
2022-04-23 01:11:50 -0600

    Create DetalleVenta.php

---
+++ app/Models/DetalleVenta.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleVenta extends Model
+{
+    use HasFactory;
+
+    public function producto()
+    {
+        return $this->hasOne(Producto::class,'id','IdProducto');
+    }
+
+    public function presentacion(){
+        return $this->belongsTo(Presentacion::class, 'IdPresentacion');
+    }
+
+    public function precio(){
+        return $this->belongsTo(Precio::class, 'IdPrecio');
+    }
+}

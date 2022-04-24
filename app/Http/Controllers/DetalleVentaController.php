a78968ba057fcc47335b52676e87954f5396569a
maryury-chacon <maryury.chacon@unah.hn>
2022-04-23 23:12:25 -0600

    Funcion de editar detalle de venta

--- app/Http/Controllers/DetalleVentaController.php
+++ app/Http/Controllers/DetalleVentaController.php
@@ -53,6 +53,54 @@ class DetalleVentaController extends Controller
         return redirect()->route('ventas.crear');
     }
 
+    public function agregar_detalle_edit(Request $request)
+    {
+        $max=0;
+        $lim = $request->IdProducto;
+        $limite = DB::table('inventarios')->where('IdProducto', '=', $lim)->get();
+        foreach($limite as $l){
+            $max = $l->Existencia;
+        }
+
+        $rules = [
+            'IdCategoria' => 'required|exists:categorias,id',
+            'IdProducto' => 'required|exists:productos,id',
+            'IdPresentacion' => 'required|exists:presentacions,id',
+            'Precio_venta' => 'required|numeric|min:1',
+            'Cantidad' => 'required|numeric|min:1|max:'.($max),
+        ];
+
+        $mensaje = [
+            'IdCategoria.required' => 'El campo categoria es obligatorio.',
+            'IdProducto.required' => 'El campo producto es obligatorio.',
+            'IdPresentacion.required' => 'El campo presentación es obligatorio.',
+            'Precio_venta.min' => 'El artículo no tiene precio establecido',
+            'Cantidad.max' => 'No hay suficiente producto para realizar esta venta'
+
+        ];
+        $this->validate($request, $rules, $mensaje);
+
+        $detalle = DetalleVenta::findOrFail($request->input('IdDetalle'));
+
+        $inve =  Inventario::where('IdProducto', '=', $request->input('IdProducto'))
+            ->where('IdPresentacion', '=', $request->input('IdPresentacion'))->firstOrFail();
+
+        $inve->Existencia = $inve->Existencia + $detalle->Cantidad;
+
+        $detalle->IdVenta = 0;
+        $detalle->IdProducto = $request->input('IdProducto');
+        $detalle->IdPresentacion = $request->input('IdPresentacion');
+        $detalle->Cantidad = $request->input('Cantidad');
+        $detalle->Precio_venta = $request->input('Precio_venta');
+        $detalle->save();
+
+        $inve->Existencia = $inve->Existencia - $request->input('Cantidad');
+
+        $inve->save();
+
+        return redirect()->route('ventas.crear');
+    }
+
     public function destroy($id)
     {
         $detalles = DetalleVenta::findOrFail($id);
@@ -68,5 +116,5 @@ class DetalleVentaController extends Controller
 
         return redirect()->route('ventas.crear');
     }
-   
+
 }

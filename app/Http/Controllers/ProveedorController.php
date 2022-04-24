fe173aa3feb5a650f21d8d3ec436d4803429a131
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-04-23 22:28:31 -0600

    Update ProveedorController.php

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -64,6 +64,41 @@ class ProveedorController extends Controller
         }
     }
     
+    public function crear2(){
+        return view('Compras.formularioProveedor');
+    }
+
+    public function store2(Request $request){
+        //Validar
+        $request->validate([
+            'EmpresaProveedora'=>'required|unique:proveedors|max:40',
+            'DirecciónDeLaEmpresa'=>'required|max:150',
+            'CorreoElectrónicoDeLaEmpresa'=>'nullable|email|unique:proveedors|max:40',
+            'TeléfonoDeLaEmpresa'=>'required',
+            'NombresDelEncargado'=>'required||max:30',
+            'ApellidosDelEncargado'=>'required|max:40',
+            'TeléfonoDelEncargado'=>'required',
+        ]);
+
+        //Formulario
+        $nuevoProveedor = new Proveedor();
+        $nuevoProveedor->EmpresaProveedora = $request->input('EmpresaProveedora');
+        $nuevoProveedor->DirecciónDeLaEmpresa = $request->input('DirecciónDeLaEmpresa');
+        $nuevoProveedor->CorreoElectrónicoDeLaEmpresa = $request->input('CorreoElectrónicoDeLaEmpresa');
+        $nuevoProveedor->TeléfonoDeLaEmpresa = $request->input('TeléfonoDeLaEmpresa');
+        $nuevoProveedor->NombresDelEncargado = $request->input('NombresDelEncargado');
+        $nuevoProveedor->ApellidosDelEncargado = $request->input('ApellidosDelEncargado');
+        $nuevoProveedor->TeléfonoDelEncargado = $request->input('TeléfonoDelEncargado');
+        $creado = $nuevoProveedor->save();
+
+        if($creado){
+            return redirect()->route('compras.crear')
+                ->with('mensaje', 'El proveedor fue creado exitosamente');
+        }else{
+            //retornar con un mensaje de error
+        }
+    }
+    
   //funcion para editar los datos
      public function edit($id){
         $proveedor = Proveedor::findOrFail($id);

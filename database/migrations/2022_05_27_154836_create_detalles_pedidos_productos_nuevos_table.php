1e0f84bf09bdf84a3b82d5adc5171ca13922087b
EstefaniHerrera <estefani.herrera@unah.hn>
2022-06-08 20:52:07 -0600

    Create 2022_05_27_154836_create_detalles_pedidos_productos_nuevos_table.php

---
+++ database/migrations/2022_05_27_154836_create_detalles_pedidos_productos_nuevos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalles_pedidos_productos_nuevos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('IdPedido')->nullable();
+            $table->string('Producto');
+            $table->string('Presentacion');
+            $table->integer('Cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalles_pedidos_productos_nuevos');
+    }
+};

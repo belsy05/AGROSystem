fe5167f73189a78ddf9a47d7041bb2c962797e23
belsy05 <belsy.mairena@unah.hn>
2022-06-13 20:40:36 -0600

    Update DetallesPedidosClientesController.php

--- app/Http/Controllers/DetallesPedidosClientesController.php
+++ app/Http/Controllers/DetallesPedidosClientesController.php
@@ -10,7 +10,66 @@ use Illuminate\Support\Facades\DB;
 class DetallesPedidosClientesController extends Controller
 {
     //
-   
+    public function agregar_detalle(Request $request)
+    {
+        $max=0;
+        $lim = $request->IdProducto;
+        $limite = DB::table('inventarios')->where('IdProducto', '=', $lim)->get();
+        foreach($limite as $l){
+            $max = $l->Existencia;
+        }
+
+        $rules = [
+            'IdCategoria' => 'required|exists:categorias,id',
+            'IdProducto' => 'required|exists:productos,id',
+            'IdPresentacion' => 'required|exists:presentacions,id',
+            'Cantidad' => 'required|numeric|min:1|max:'.($max),
+        ];
+
+        $mensaje = [
+            'IdCategoria.required' => 'El campo categoria es obligatorio.',
+            'IdProducto.required' => 'El campo producto es obligatorio.',
+            'IdPresentacion.required' => 'El campo presentación es obligatorio.',
+            'Cantidad.max' => 'No hay suficiente producto para realizar este pedido',
+        ];
+        $this->validate($request, $rules, $mensaje);
+
+        $existe = DB::table('detalles_pedidos_clientes')->where('IdProducto', '=', $request->IdProducto)
+                                                            ->where('IdPresentacion', '=', $request->IdPresentacion)
+                                                            ->where('IdVenta', '=', 0)->exists();
+
+        if ($existe) {
+            $detalle = DetallesPedidosClientes::where('IdProducto', '=', $request->IdProducto)
+                                ->where('IdPresentacion', '=', $request->IdPresentacion)
+                                ->where('IdVenta', '=', 0)->firstOrFail();
+                        
+            $detalle->IdVenta = 0;
+            $detalle->IdProducto = $request->input('IdProducto');
+            $detalle->IdPresentacion = $request->input('IdPresentacion');
+            $detalle->Cantidad = $detalle->Cantidad + $request->input('Cantidad');
+            $detalle->save();
+        } else {
+            $detalle = new DetallesPedidosClientes();
+            $detalle->IdVenta = 0;
+            $detalle->IdProducto = $request->input('IdProducto');
+            $detalle->IdPresentacion = $request->input('IdPresentacion');
+            $detalle->Cantidad = $request->input('Cantidad');
+            $detalle->save();
+        }
+
+
+        return redirect()->route('pedidosCliente.crear');
+    }
+
+    public function destroy($id)
+    {
+        $detalles = DetallesPedidosClientes::findOrFail($id);
+
+
+        $detalles->delete();
+
+        return redirect()->route('pedidosCliente.crear');
+    }
 
     public function agregar_detalle_edit(Request $request)
     { 

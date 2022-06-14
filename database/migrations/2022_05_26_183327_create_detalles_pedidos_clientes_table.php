ec08820ed950adae8f7d5bb905f459e82ec5f8f4
belsy05 <belsy.mairena@unah.hn>
2022-06-13 20:27:46 -0600

    Create 2022_05_26_183327_create_detalles_pedidos_clientes_table.php

---
+++ database/migrations/2022_05_26_183327_create_detalles_pedidos_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalles_pedidos_clientes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('IdVenta')->nullable();
+            $table->unsignedBigInteger('IdProducto');
+            $table->foreign('IdProducto')->references('id')->on('productos');
+            $table->unsignedBigInteger('IdPresentacion');
+            $table->foreign('IdPresentacion')->references('id')->on('presentacions');
+            $table->integer('Cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalles_pedidos_clientes');
+    }
+};

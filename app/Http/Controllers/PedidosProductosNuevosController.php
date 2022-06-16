9d4127617dff42d8eca5397e1948e145f54a6e92
MarilynMolina <mjmolinar@unah.hn>
2022-06-16 10:41:41 -0600

    H85 e H86

--- app/Http/Controllers/PedidosProductosNuevosController.php
+++ app/Http/Controllers/PedidosProductosNuevosController.php
@@ -12,6 +12,33 @@ class PedidosProductosNuevosController extends Controller
 {
     //
 
+    public function index(Request $request)
+    {
+        $cliente = "";
+        $cliente = $request->input('texto');
+        $total_cantidad = 0;
+
+        $pedidos = PedidosProductosNuevos::select('pedidos_productos_nuevos.*', 'clientes.NombresDelCliente', 'clientes.ApellidosDelCliente')
+            ->join('clientes', 'clientes.id', '=', 'pedidos_productos_nuevos.cliente_id')
+            ->where('NombresDelCliente', 'like', '%' . $cliente . '%')
+            ->orwhere('ApellidosDelCliente', 'like', '%' . $cliente . '%')
+            ->paginate(15);
+            foreach ($pedidos  as $key => $value) {
+                $value->cliente = Cliente::findOrFail($value->cliente_id);  
+                
+            }
+            
+
+        return view('Ventas.indexPedidosProductosNuevos')->with('pedidos', $pedidos)->with('cliente', $cliente) ->with('total_cantidad', $total_cantidad);
+    }
+
+    public function show($id)
+    {
+        $pedidos = PedidosProductosNuevos::findOrFail($id);
+        $details = DetallesPedidosProductosNuevos::where('IdPedido', $id)->get();
+        return view('Ventas.detallePedidosProductosNuevos')->with('pedidos', $pedidos)->with('detalles', $details);
+    }
+
     public function create()
     {
         $total_cantidad = 0;
@@ -83,4 +110,19 @@ class PedidosProductosNuevosController extends Controller
         return redirect()->route('pedidosClienteP.crear');
     }
 
+    public function updateStatus($id)
+    {
+        $detalles =  DetallesPedidosProductosNuevos::where('IdPedido', '=', $id)->get();
+        foreach ($detalles as $key => $value) {
+
+            DetallesPedidosProductosNuevos::destroy($value->id);
+        }
+
+        PedidosProductosNuevos::destroy($id);
+
+        return redirect()->route('pedidosClienteP.index');
+
+        
+
+    }
 }
--- resources/views/Ventas/formularioPedidosProductosNuevos.blade.php
+++ resources/views/Ventas/formularioPedidosProductosNuevos.blade.php
@@ -194,8 +194,8 @@
                                     <label style="width: 100%" for="">Cantidad</label>
                                     <input style="width: 100%" type="text" name="Cantidad"
                                         class="form-control {{ $errors->has('Cantidad') ? 'is-invalid' : '' }}" 
-                                        value="{{ old('Cantidad') }}" id="Cantidad"
-                                        title="Ingrese cantidad de la compra en números." maxlength="4" pattern="[0-9]+" required placeholder="0">
+                                        value="{{ old('Cantidad', 0) }}" id="Cantidad"
+                                        title="Ingrese cantidad de la compra en números." maxlength="4" pattern="[0-9]+" required>
                                 </div>
                             </div>
                         </div>
@@ -209,10 +209,73 @@
         </div>
     </div>
 
+    {{-- Modal de editar los detalles --}}
+    <div class="modal fade" id="editar_detalleP" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form action="{{ route('detalle_pedidoP.editar') }}" method="POST">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Editar detalles</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="row" style="width: 100%">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Producto</label>
+                                    <input type="text" class="form-control" name="NombreDelProducto"
+                                        id="e_NombreDelProducto" required placeholder="Nombre del producto" maxlength="40"
+                                        value="{{ old('NombreDelProducto') }}">
+                                </div>
+                                <input type="text" name="IdDetalle" id="e_IdDetalle" hidden>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Presentación</label>
+                                    <input type='text' class='form-control' name='presentacion' id='e_presentacion'
+                                        placeholder='Presentacion' value="{{ old('presentacion[]') }}" maxlength='30'
+                                        required>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="width: 100%">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label style="width: 100%" for="">Cantidad</label>
+                                    <input style="width: 100%" type="text" name="Cantidad"
+                                        class="form-control {{ $errors->has('Cantidad') ? 'is-invalid' : '' }}"
+                                        value="{{ old('Cantidad', 0) }}" id="e_Cantidad" required
+                                        title="Ingrese cantidad de la compra en números." maxlength="4" pattern="[0-9]+">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <button type="submit" class="btn btn-primary">Actualizar</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('js')
     @push('alertas')
         <script>
+            function editar_detalleP(Producto, Presentacion, Cantidad, id) {
+                $('#e_NombreDelProducto').val(Producto);
+                $('#e_presentacion').val(Presentacion);
+                $('#e_Cantidad').val(Cantidad);
+                $('#e_IdDetalle').val(id);
+
+            }
 
             function confirmar() {
                 var formul = document.getElementById("form_guardarP");

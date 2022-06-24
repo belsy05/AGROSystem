4bbc5345bcccdaa403139939dad6b5be79fa2c51
belsy05 <belsy.mairena@unah.hn>
2022-06-23 19:06:48 -0600

    Update PedidosClientesController.php

--- app/Http/Controllers/PedidosClientesController.php
+++ app/Http/Controllers/PedidosClientesController.php
@@ -32,73 +32,7 @@ class PedidosClientesController extends Controller
         return view('Ventas.indexPedidosCliente')->with('pedidos', $pedidos)->with('cliente', $cliente)->with('total_cantidad', $total_cantidad);
     }
 
-    public function show($id)
-    {
-        $pedidos = PedidosClientes::where('id', $id)->first();
-        $details = DetallesPedidosClientes::where('IdVenta', $pedidos->id)->get();
-        return view('Ventas.detallePedidosCliente')->with('pedidos', $pedidos)->with('detalles', $details);
-    }
 
-    public function create()
-    {
-        // ////////////////////////////////////////////////////
-        // //////////////////////////////////////////////////////////
-        $detalles =  DetallesPedidosClientes::where('IdVenta', 0)->get();
-        $productos = Producto::all();
-        $cliente = Cliente::all();
-        $categoria = Categoria::all();
-        $presentacion = Presentacion::all();
-        $inventarios = Inventario::all();
-        $total_cantidad = 0;
-
-        foreach ($detalles  as $key => $value) {
-            $total_cantidad += $value->Cantidad;
-        }
-
-        return view('Ventas.formularioPedidosCliente')
-            ->with('cliente', $cliente)
-            ->with('productos', $productos)
-            ->with('presentacion', $presentacion)
-            ->with('categoria', $categoria)
-            ->with('inventarios', $inventarios)
-            ->with('detalles', $detalles)
-            ->with('total_cantidad', $total_cantidad);
-    }
-
-
-    public function store(Request $request)
-    {
-
-
-        $request->validate([
-            'FechaPedidoCliente' => 'required|date|before:tomorrow|after:yesterday',
-            'TotalCantidad' => 'required|numeric|min:1'
-        ], [
-
-            'FechaPedidoCliente.before' => 'El campo fecha de pedido debe de ser hoy',
-            'FechaPedidoCliente.after' => 'El campo fecha de pedido debe de ser hoy',
-            'TotalCantidad.min' => 'Ingrese detalles para este pedido'
-
-        ]);
-
-
-        $venta = new PedidosClientes();
-
-        $venta->cliente_id = $request->input('Cliente');
-        $venta->FechaDelPedido = $request->input('FechaPedidoCliente');
-        $venta->save();
-
-        $detalles =  DetallesPedidosClientes::where('IdVenta', 0)->get();
-
-
-        foreach ($detalles  as $key => $value) {
-            $de = DetallesPedidosClientes::findOrFail($value->id);
-            $de->IdVenta = $venta->id;
-
-            $de->save();
-        }
-        return redirect()->route('pedidosCliente.index');
-    }
 
     public function limpiar()
     {

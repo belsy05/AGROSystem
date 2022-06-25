c935cd5a4afb90fcf75cec472c3ad22b7f589ad3
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2022-06-24 18:43:18 -0600

    Update DetallesPedidosProveedorController.php

--- app/Http/Controllers/DetallesPedidosProveedorController.php
+++ app/Http/Controllers/DetallesPedidosProveedorController.php
@@ -51,38 +51,4 @@ class DetallesPedidosProveedorController extends Controller
         return redirect()->route('pedidosProveedor.crear');
     }
 
-    public function destroy($id)
-    {
-        $detalles = DetallesPedidosProveedor::findOrFail($id);
-        $detalles->delete();
-
-        return redirect()->route('pedidosProveedor.crear');
-    }
-
-
-    public function agregar_detalle_edit(Request $request)
-    {
-        
-        $rules = [
-            'NombreDelProducto' => 'required|max:40',
-            'presentacion' => 'required|max:30',
-            'Cantidad' => 'required|numeric|min:1',
-        ];
-
-        $mensaje = [
-            'NombreDelProducto.required' => 'El campo producto es obligatorio.',
-            'presentacion.required' => 'El campo presentación es obligatorio.',
-        ];
-        $this->validate($request, $rules, $mensaje);
-
-        $detalle = DetallesPedidosProveedor::findOrFail($request->input('IdDetalle'));
-
-        $detalle->IdPedido = 0;
-        $detalle->Producto = $request->input('NombreDelProducto');
-        $detalle->Presentacion = $request->input('presentacion');
-        $detalle->Cantidad = $request->input('Cantidad');
-        $detalle->save();
-
-        return redirect()->route('pedidosProveedor.crear');
-    }
-}
+    

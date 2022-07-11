012f31a9a7e5e5f27aa983c322d92f6e171590f0
MarilynMolina <84828279+MarilynMolina@users.noreply.github.com>
2022-07-11 15:23:00 -0600

    sprint2
    detalles servicio

--- resources/views/Servicios/verServicio.blade.php
+++ resources/views/Servicios/verServicio.blade.php
@@ -2,51 +2,66 @@
 @section('titulo', 'Servicio')
 @section('contenido')
 
-<h1> Detalles del servicio para {{$servicio->NombresDelCliente}} {{$servicio->ApellidosDelCliente}}
-</h1>
-<br><br>
-<table class="table">
-    <thead class="table-secondary">
+    <h1> Detalles del técnico  {{$servicio->personal->NombresDelEmpleado}} {{$servicio->personal->ApellidosDelEmpleado}}
+    </h1>
+    <br><br>
+    <table class="table">
+        <thead class="table-secondary">
         <tr>
             <th scope="col">Campos</th>
-            <th scope="col">Información del empleado</th>
-        </tr>  
-    </thead>
-    <tbody>
+            <th scope="col">Información del técnico</th>
+        </tr>
+        </thead>
+        <tbody>
         <tr>
-            <th scope="row"> Técnico </th>
-            <td scope="col">{{ $cargo->NombreDelCargo}} </td>
+            <th scope="row">Técnico</th>
+            <td scope="col">{{$cargo->NombreDelCargo}} </td>
         </tr>
         <tr>
-            <th scope="row">Nombres</th>
-            <td scope="col">{{ $servicio->personals->NombresDelEmpleado }} {{ $servicio->personals->ApellidosDelEmpleado }} </td>
+            <th scope="row">Nombre del empleado</th>
+            <td scope="col">{{ $servicio->personal->NombresDelEmpleado }} {{ $servicio->personal->ApellidosDelEmpleado }} </td>
         </tr>
+
         <tr>
             <th scope="row">Teléfono del técnico</th>
-            <td scope="col">{{ $servicio->personals->Teléfono }} </td>
-        </tr>
-        <tr>
-            <th scope="row">Cliente</th>
-            <td scope="col">{{ $servicio->clientes->NombresDelCliente }} {{ $servicio->clientes->ApellidosDelCliente }} </td>
-        </tr>       
-        <tr>
-            <th scope="row">Teléfono del cliente</th>
-            <td scope="col">{{ $servicio->TeléfonoCliente }} </td>
+            <td scope="col">{{ $servicio->personal->Teléfono }} </td>
         </tr>
         <tr>
             <th scope="row">Fecha del servicio </th>
             <td scope="col">{{\Carbon\Carbon::parse($servicio->FechaDeRealizacion)->locale("es")->isoFormat("DD MMMM, YYYY")}} </td>
         </tr>
+        </tbody>
+    </table>
+    <br>
+    <h1> Detalles del cliente  {{$servicio->clientes->clientes }} {{ $servicio->clientes->ApellidosDelCliente}}
+    </h1>
+    <br><br>
+    <table class="table">
+        <thead class="table-secondary">
         <tr>
-            <th scope="row">Dirección</th>
-            <td scope="col">{{ $servicio->Dirección}} </td>
+            <th scope="col">Campos</th>
+            <th scope="col">Información del cliente</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">Cliente</th>
+            <td scope="col">{{ $servicio->clientes->clientes }} {{ $servicio->clientes->ApellidosDelCliente }} </td>
+        </tr>
+        <tr>
+            <th scope="row">Teléfono del cliente</th>
+            <td scope="col">{{ $servicio->TeléfonoCliente }} </td>
+        </tr>
+        <tr>
+            <th scope="row">Descripción</th>
+            <td scope="col">{{ $servicio->DescripciónDelServicio}} </td>
         </tr>
         <tr>
             <th scope="row">Dirección</th>
             <td scope="col">{{ $servicio->Dirección}} </td>
         </tr>
-    </tbody>
-</table>
+        </tbody>
+    </table>
 
-<a class="btn btn-primary" href="{{route('servicio.index')}}"> Regresar </a>
+    <a class="btn btn-primary" href="{{route('servicio.index')}}"> Regresar </a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -472,7 +472,7 @@ Route::get('/Servicio/buscar', [ServicioController::class, 'index2'])
 ->name('servicio.index2');
 
 Route::get('/Servicio/{id}', [ServicioController::class, 'show'])
-->name('servicio.mostrar')->where('id', '[0-9]+');
+    ->name('servicio.mostrar')->where('id', '[0-9]+');
 
 Route::get('/Servicio/crear', [ServicioController::class, 'crear'])
 ->name('servicio.crear');

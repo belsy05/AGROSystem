a4d7cf1f8971e8c099b112288694e3bb9789df42
EstefaniHerrera <estefani.herrera@unah.hn>
2022-07-14 14:28:40 -0600

    Update DetallesPedidosProveedorController.php

--- app/Http/Controllers/DetallesPedidosProveedorController.php
+++ app/Http/Controllers/DetallesPedidosProveedorController.php
@@ -106,4 +106,144 @@ class DetallesPedidosProveedorController extends Controller
 
         return redirect()->route('pedidosProveedor.crear');
     }
+
+    public function edit_agregar_detalle($id, Request $request)
+    {
+
+        $rules = [
+            'NombreDelProducto' => 'required|max:40',
+            'presentacion' => 'required|max:30',
+            'Cantidad' => 'required|numeric|min:1',
+        ];
+
+        $mensaje = [
+            'NombreDelProducto.required' => 'El campo producto es obligatorio.',
+            'presentacion.required' => 'El campo presentación es obligatorio.',
+        ];
+        $this->validate($request, $rules, $mensaje);
+
+        $existe = DB::table('detalles_pedidos_proveedores_temporals')->where('IdPedido', '=', $id)
+                                                            ->where('Producto', '=', $request->NombreDelProducto)
+                                                            ->where('Presentacion', '=', $request->presentacion)->exists();
+
+        $exis = DB::table('detalles_pedidos_proveedores_temporals')->where('IdPedido', '=', 0)
+                                                            ->where('Producto', '=', $request->NombreDelProducto)
+                                                            ->where('Presentacion', '=', $request->presentacion)->exists();
+
+        if ($existe) {
+            $det = DetallesPedidosProveedoresTemporal::where('IdPedido', '=', $id)
+                                                ->where('Producto', '=', $request->NombreDelProducto)
+                                                ->where('Presentacion', '=', $request->presentacion)->firstOrFail();
+
+            $det->IdPedido = $det->IdPedido;
+            $det->Producto = $request->input('NombreDelProducto');
+            $det->Presentacion = $request->input('presentacion');
+            $det->Cantidad = $det->Cantidad + $request->input('Cantidad');
+            $det->save();
+        }else{
+            if($exis){
+                $det = DetallesPedidosProveedoresTemporal::where('IdPedido', '=', 0)
+                                                ->where('Producto', '=', $request->NombreDelProducto)
+                                                ->where('Presentacion', '=', $request->presentacion)->firstOrFail();
+
+                $det->IdPedido = $det->IdPedido;
+                $det->Producto = $request->input('NombreDelProducto');
+                $det->Presentacion = $request->input('presentacion');
+                $det->Cantidad = $det->Cantidad + $request->input('Cantidad');
+                $det->save();
+            }else {
+                $detalle = new DetallesPedidosProveedoresTemporal();
+                $detalle->IdPedido = 0;
+                $detalle->Producto = $request->input('NombreDelProducto');
+                $detalle->Presentacion = $request->input('presentacion');
+                $detalle->Cantidad = $request->input('Cantidad');
+                $detalle->save();
+            }
+        }
+
+
+        return redirect()->route('pedidosProveedor.edit', ['id' => $id]);
+    }
+
+    public function destroy2($idDetalle, $id)
+    {
+        $detalles = DetallesPedidosProveedoresTemporal::findOrFail($idDetalle);
+        $detalles->IdPedido = null;
+        $detalles->save();
+
+        return redirect()->route('pedidosProveedor.edit', ['id' => $id]);
+    }
+
+
+    public function edit_agregar_detalle_edit($id, Request $request)
+    {
+       
+        
+        $rules = [
+            'NombreDelProducto' => 'required|max:40',
+            'presentacion' => 'required|max:30',
+            'Cantidad' => 'required|numeric|min:1',
+        ];
+
+        $mensaje = [
+            'NombreDelProducto.required' => 'El campo producto es obligatorio.',
+            'presentacion.required' => 'El campo presentación es obligatorio.',
+        ];
+        $this->validate($request, $rules, $mensaje);
+
+        $existe = DB::table('detalles_pedidos_proveedores_temporals')->where('IdPedido', '=', $id)
+                                                            ->where('Producto', '=', $request->NombreDelProducto)
+                                                            ->where('Presentacion', '=', $request->presentacion)
+                                                            ->where('id', '!=', $request->input('IdDetalle'))->exists();
+
+        $exis = DB::table('detalles_pedidos_proveedores_temporals')->where('IdPedido', '=', 0)
+                                                            ->where('Producto', '=', $request->NombreDelProducto)
+                                                            ->where('Presentacion', '=', $request->presentacion)
+                                                            ->where('id', '!=', $request->input('IdDetalle'))->exists();
+
+        if ($existe) {
+            $det = DetallesPedidosProveedoresTemporal::where('IdPedido', '=', $id)
+                                                ->where('Producto', '=', $request->NombreDelProducto)
+                                                ->where('Presentacion', '=', $request->presentacion)
+                                                ->where('id', '!=', $request->input('IdDetalle'))->firstOrFail();
+
+            $det->IdPedido = $det->IdPedido;
+            $det->Producto = $request->input('NombreDelProducto');
+            $det->Presentacion = $request->input('presentacion');
+            $det->Cantidad = $det->Cantidad + $request->input('Cantidad');
+            $det->save();
+
+            $detalles = DetallesPedidosProveedoresTemporal::findOrFail($request->input('IdDetalle'));
+            $detalles->IdPedido = null;
+            $detalles->save();
+        }else{
+            if($exis){
+                $det = DetallesPedidosProveedoresTemporal::where('IdPedido', '=', 0)
+                                                ->where('Producto', '=', $request->NombreDelProducto)
+                                                ->where('Presentacion', '=', $request->presentacion)
+                                                ->where('id', '!=', $request->input('IdDetalle'))->firstOrFail();
+
+                $det->IdPedido = $det->IdPedido;
+                $det->Producto = $request->input('NombreDelProducto');
+                $det->Presentacion = $request->input('presentacion');
+                $det->Cantidad = $det->Cantidad + $request->input('Cantidad');
+                $det->save();
+
+                $detalles = DetallesPedidosProveedoresTemporal::findOrFail($request->input('IdDetalle'));
+                $detalles->IdPedido = null;
+                $detalles->save();
+            } else {
+                    $detalle = DetallesPedidosProveedoresTemporal::findOrFail($request->input('IdDetalle'));
+
+                    $detalle->IdPedido = $detalle->IdPedido;
+                    $detalle->Producto = $request->input('NombreDelProducto');
+                    $detalle->Presentacion = $request->input('presentacion');
+                    $detalle->Cantidad = $request->input('Cantidad');
+                    $detalle->save();
+            }
+            
+        }
+
+        return redirect()->route('pedidosProveedor.edit', ['id' => $id]);
+    }
 }
